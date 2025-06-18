8c01488447466b9ff2e745ab2177199d081de80b
Nancy-007 <gurdevnancy83@gmail.com>
2025-06-18 23:44:45 +0530

    Fix casing of AlbumItem.jsx import

--- src/components/displayHome.jsx
+++ src/components/displayHome.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './navbar'
 import { albumsData} from '../assets/assets'
-import AlbumItem from './AlbumItemTemp'
+import AlbumItem from './AlbumItem'
 
 import {songsData} from '../assets/assets'
 import SongItem from './SongItem'
